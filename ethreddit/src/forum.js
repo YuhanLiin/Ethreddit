497dcacc8d8be746158ed535e2e84ae41ba34cd1
YuhanLiin <linyuhan0315@hotmail.com>
2018-09-15 09:17:25 -0400

    Integrated react app

---
+++ ethreddit/src/forum.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+import * as contractData from '../../build/contracts/Forum.json';
+
+const address = '0x28e508bb059ebd2529d8844b5ce0b7b9493875fa';
+
+const abi = contractData.abi;
+
